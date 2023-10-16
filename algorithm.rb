20b5810d098e3f9e5fabd466c6261d53ebb4e601
nanamuramarina <nanamuramarina@gmail.com>
2023-10-16 13:50:03 +0000

    クラス

---
+++ algorithm.rb
@@ -0,0 +1,5 @@
+A1 = gets.to_i
+A2 = gets.to_i
+A3 = gets.to_i
+
+puts A1*A2*A3
---
+++ new.rb
@@ -0,0 +1,28 @@
+class Car
+  def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+  
+  def turn(direction)
+    puts "車で#{direction}キロ走ります。"
+  end
+  
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+car = Car.new
+car.move("右", 5)
+
+car = Car.new
+car.run(5)
+
+class Car
+  def self.run(distance)
+     puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
